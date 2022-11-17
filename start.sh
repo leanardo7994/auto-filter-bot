dec7f5758df2ee187da8b7789617146d8c45ce3c
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2022-11-17 09:59:41 +0530

    Koyeb support final

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BUDDY1609/koyeb-auto-filter-bot /vps
+  git clone https://github.com/BUDDY1609/auto-filter-bot /vps
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /vps
