e2feec489f157d0494b933cb345ad96558d3c733
ｒｙｍｅ <118106830+ry-me@users.noreply.github.com>
2022-11-19 13:38:33 +0530

    Balalalalal

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,6 @@
-# Kanged From @TroJanZheX
+#@iPrimeHub™
+
+
 import asyncio
 import re
 import ast
@@ -65,8 +67,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                    url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=files_{file.file_id}")
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}')
                 ),
             ]
             for file in files
@@ -653,8 +654,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                    url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=pre_{file.file_id}")
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}')
                 ),
             ]
             for file in files
@@ -663,12 +663,10 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}",
-                    url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=pre_{file.file_id}")
+                    text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}')
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
-                    callback_data=f'{pre}_#{file.file_id}',
+                    text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
             for file in files
