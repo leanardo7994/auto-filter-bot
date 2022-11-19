8f40e597f224f9f1d4cea23653c41a5fc685dcf0
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-11-19 09:17:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -42,7 +42,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("⚠️ Hey Don't Click Others Requested Files. Please Request At Yourself", show_alert=True)
     try:
         offset = int(offset)
     except:
